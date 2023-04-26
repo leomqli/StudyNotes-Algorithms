7c416307dbc47475a96d4169ae0be132ef1ca025
Leo <leomqli@gmail.com>
2023-04-26 16:21:52 +0800

    feat: 剑指 Offer 24. 反转链表

--- Algorithms/LeetCode/ListNode/206.反转链表/206.ReverseList.swift
+++ Algorithms/LeetCode/ListNode/206.反转链表/206.ReverseList.swift
@@ -19,6 +19,14 @@ extension Algorithm {
 
 extension Solution {
 	
+	func reverseList(_ head: ListNode?) -> ListNode? {
+		
+		print(reverseList1(head))
+		print(reverseList2(head))
+		
+		return nil
+	}
+	
 	/// 方法一：迭代
 	///
 	/// 思路
@@ -27,7 +35,7 @@ extension Solution {
 	/// 在遍历链表时，将当前节点的 next 指针改为指向前一个节点。由于节点没有引用其前一个节点，因此必须事先存储其前一个节点。在更改引用之前，还需要存储后一个节点。最后返回新的头引用。
 	///
 	/// 复杂度分析
-	/// 时间复杂度：O(n)，其中 nnn 是链表的长度。需要遍历链表一次。
+	/// 时间复杂度：O(n)，其中 n 是链表的长度。需要遍历链表一次。
 	/// 空间复杂度：O(1)。
 	///
 	///	结果
@@ -36,7 +44,7 @@ extension Solution {
 	///
 	/// - Parameter head: 头节点
 	/// - Returns: 链表
-    func reverseList(_ head: ListNode?) -> ListNode? {
+    func reverseList1(_ head: ListNode?) -> ListNode? {
         
 		if head == nil || head?.next == nil {
             return head
@@ -58,4 +66,30 @@ extension Solution {
         
         return pre
     }
+	
+	
+	/// 方法二：递归
+	///
+	/// 思路
+	///	遍历节点，先将当前节点的下一节点保存，然后将下一节点指向上一节点，然后将当前节点与下一节点继续进行递归
+	///	递归终止条件：遍历至尾节点时，下一节点为nil
+	///
+	/// 复杂度分析
+	/// 时间复杂度：O(n)，其中 n 是链表的长度。需要遍历链表一次。
+	/// 空间复杂度：O(n)。
+	///
+	///	结果
+	/// 时间 12 ms 击败 60%
+	/// 内存 14.6 MB 击败 36.67%
+	func reverseList2(_ head: ListNode?) -> ListNode? {
+		return recursiveMethod(nil, cur: head)
+	}
+
+	private func recursiveMethod(_ pre: ListNode?, cur: ListNode?) -> ListNode? {
+		guard cur != nil else { return pre }
+		let temp: ListNode? = cur?.next
+		cur?.next = pre
+		return recursiveMethod(cur, cur: temp)
+	}
+	
 }
---
+++ Algorithms/剑指Offer/ListNode/剑指 Offer 24. 反转链表.swift
@@ -0,0 +1,10 @@
+//
+//  剑指 Offer 24. 反转链表.swift
+//  Algorithms
+//
+//  Created by Leo on 2023/4/26.
+//
+// 	与 LeetCode/ListNode/206.反转链表/206.ReverseList.swift 是同一道题目，这里省略
+
+import Foundation
+
